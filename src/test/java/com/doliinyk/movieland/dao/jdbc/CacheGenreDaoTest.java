ccb57c2544a4b61450ada7782cf4ab9e36e2c805
Dmytro Oliinyk <dmitry.oleynik@gmail.com>
2018-11-26 14:19:11 +0200

    [t-1] create genre cache

---
+++ src/test/java/com/doliinyk/movieland/dao/jdbc/CacheGenreDaoTest.java
@@ -0,0 +1,26 @@
+package com.doliinyk.movieland.dao.jdbc;
+
+import com.doliinyk.movieland.dao.GenreDao;
+import com.doliinyk.movieland.entity.Genre;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(locations = "classpath:testContext.xml")
+public class CacheGenreDaoTest {
+    @Autowired
+    private GenreDao cacheGenreDao;
+
+    @Test
+    public void getAll() {
+        List<Genre> genres = cacheGenreDao.getAll();
+        assertEquals(3, genres.size());
+    }
+}
--- src/test/java/com/doliinyk/movieland/web/controller/GenreControllerITest.java
+++ src/test/java/com/doliinyk/movieland/web/controller/GenreControllerITest.java
@@ -6,7 +6,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -19,11 +20,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+//@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 @ContextConfiguration(locations = {
         "file:src/main/webapp/WEB-INF/applicationContext.xml",
         "file:src/main/webapp/WEB-INF/dispatcherServlet-servlet.xml",
-        "file:src/test/resources/testContext.xml"
+        "classpath:testContext.xml"
 })
 @WebAppConfiguration
 public class GenreControllerITest {
